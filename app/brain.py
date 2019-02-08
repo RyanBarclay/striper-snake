170e982e8c239fcbf4b247017818dd4404abee2d
RyanBarclay <mrryanbarclay@gmail.com>
2019-02-08 10:34:38 -0800

    Fixed look funtion
    Won't put tail as the body and body as the tail

--- app/brain.py
+++ app/brain.py
@@ -3,7 +3,8 @@ import json
 import numpy as np
 
 debug = True
-
+# global_temp_int_1 = 0
+# global_temp_int_2 = 0 
 
 def think(data):
     #this is where the work starts
@@ -82,22 +83,23 @@ def look(board_food, board_snakes, you_id, board_matrix, turn):
             if cur_snake_id == you_id:
                 if i == 0:
                     board_matrix[cur_snake_body_y][cur_snake_body_x] ='mh'
-                elif i== cur_snake_body_length-1:
+                elif i == cur_snake_body_length-1:
                     if turn == 0:
                         board_matrix[cur_snake_body_y][cur_snake_body_x] ='mh'
-                    elif turn == 1:
-                        board_matrix[cur_snake_body_y][cur_snake_body_x] ='mt'
                     else:
-                        board_matrix[cur_snake_body_y][cur_snake_body_x] ='mb'
+                        board_matrix[cur_snake_body_y][cur_snake_body_x] ='mt'
                 else:
-                    board_matrix[cur_snake_body_y][cur_snake_body_x] ='mt'
+                    board_matrix[cur_snake_body_y][cur_snake_body_x] ='mb'
             else:
                 if i == 0:
                     board_matrix[cur_snake_body_y][cur_snake_body_x] ='h'
-                elif i== cur_snake_body_length-1:
-                    board_matrix[cur_snake_body_y][cur_snake_body_x] ='b'
+                elif i == cur_snake_body_length-1:
+                    if turn == 0:
+                        board_matrix[cur_snake_body_y][cur_snake_body_x] ='h'
+                    else:
+                        board_matrix[cur_snake_body_y][cur_snake_body_x] ='t'
                 else:
-                    board_matrix[cur_snake_body_y][cur_snake_body_x] ='t'
+                    board_matrix[cur_snake_body_y][cur_snake_body_x] ='b'
             i += 1
         board_snakes_amount -= 1
     #end of look funtion
@@ -105,30 +107,42 @@ def look(board_food, board_snakes, you_id, board_matrix, turn):
 def instincts(board_matrix):
 
     # will find my head and tell me where it is and exit x,y
-    x,y = 0,0
+    head_pos_x = 0
+    head_pos_y = 0
     element = 'mh'
-    find_element_index_2d(board_matrix,element,x,y)
+    find_element_index_2d(board_matrix, element, head_pos_x,head_pos_y)
+    print head_pos_x
+    print head_pos_y
 
     choice= ['up','down','left','right']
 
-    if debug == True:
-        if x == -1:
-            print "find_element_index_2d failed"
 
     return choice[0]
 
-def find_element_index_2d(matrix,element,x,y):
-    x= len(matrix) - 1
+def find_element_index_2d(matrix,element,x_input,y_input):
+    y = len(matrix) - 1
     temp_token = matrix[0]
-    y= len(temp_token) - 1
+    x = len(temp_token) - 1
     end = False
-    while x >= 0:
-        while y >= 0:
-            if matrix[y][x]== element:
+    while y >= 0:
+        while x >= 0:
+            temp_element = matrix[y][x]
+            if temp_element == element:
                 end = True
-            else:
-                y -= 1
-        if end:
-            break
-        else:
+            if end == True:
+                break
             x -= 1
+        if end == True:
+            #when it finds head location do this
+            x_input = x
+            y_input = y
+            break
+        x= len(temp_token) - 1
+        y -= 1
+
+    if debug == True:
+        if y == -1:
+            print "ERROR element not found in find_element_index_2d"
+        else:
+            print "find_element_index_2d Working"
+            print "x:%d y:%d"%(x,y)
