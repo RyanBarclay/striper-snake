c5e78cb5817551628f93abea04561a58e1aa2c3a
RyanBarclay <mrryanbarclay@gmail.com>
2019-02-09 20:47:28 -0800

    Kinda half working
    Gotta fix werid bug with hunting choices

--- app/brain.py
+++ app/brain.py
@@ -3,8 +3,6 @@ import json
 import numpy as np
 
 debug = True
-# global_temp_int_1 = 0
-# global_temp_int_2 = 0 
 
 def think(data):
     #this is where the work starts
@@ -32,9 +30,9 @@ def think(data):
     board_matrix = [[0 for x in range(board_width)] for y in range(board_height)]
     # this will make an array elements of widths. ie
     look(board_food, board_snakes, you_id, board_matrix, turn)
+    direction = instincts(board_matrix, board_food)
     if debug == True:
         print(np.matrix(board_matrix))
-    direction = instincts(board_matrix)
     return direction
 
 def look(board_food, board_snakes, you_id, board_matrix, turn):
@@ -104,45 +102,204 @@ def look(board_food, board_snakes, you_id, board_matrix, turn):
         board_snakes_amount -= 1
     #end of look funtion
 
-def instincts(board_matrix):
+def instincts(board_matrix, board_food):
 
     # will find my head and tell me where it is and exit x,y
-    head_pos_x = 0
-    head_pos_y = 0
+    head_pos_y = len(board_matrix)-1
+    head_token = board_matrix[0]
+    head_pos_x = len(head_token)-1
+    x_max = head_pos_x
     element = 'mh'
-    find_element_index_2d(board_matrix, element, head_pos_x,head_pos_y)
-    print head_pos_x
-    print head_pos_y
-
-    choice= ['up','down','left','right']
-
-
-    return choice[0]
-
-def find_element_index_2d(matrix,element,x_input,y_input):
-    y = len(matrix) - 1
-    temp_token = matrix[0]
-    x = len(temp_token) - 1
     end = False
-    while y >= 0:
-        while x >= 0:
-            temp_element = matrix[y][x]
-            if temp_element == element:
+    safe_choices = []
+
+    # this finds the head .... hopefully no bugs
+    while head_pos_y >= 0:
+        while head_pos_x >= 0:
+            if board_matrix[head_pos_y][head_pos_x] == element:
                 end = True
-            if end == True:
                 break
-            x -= 1
+            else:
+                head_pos_x -= 1
         if end == True:
-            #when it finds head location do this
-            x_input = x
-            y_input = y
             break
-        x= len(temp_token) - 1
-        y -= 1
+        else:
+            head_pos_y -= 1
+            head_pos_x = x_max
+    # end of find head loop
 
     if debug == True:
-        if y == -1:
-            print "ERROR element not found in find_element_index_2d"
+        print "head of snake x:%d ,y:%d" %(head_pos_x,head_pos_y)
+
+    safe_choices = move_check(safe_choices, head_pos_x, head_pos_y, board_matrix)
+
+
+    return hunting(safe_choices, board_food,head_pos_x,head_pos_y)
+
+def move_check(safe_choices, head_pos_x, head_pos_y, board_matrix):
+    """
+    This will return a pool of moves that are acceptable as moves and return it
+    as a list of those moves as strings acceptable as /move entries
+    """
+
+    temp_token = board_matrix[0]
+
+    top_edge = len(board_matrix)-1
+    right_edge = len(temp_token)-1
+    left_edge = 0
+    bottom_edge = 0
+
+    up_clear = True
+    down_clear = True
+    left_clear = True
+    right_clear = True
+
+    danger_elements = ['h','b','mb','t','mt']
+
+
+    #if up is a bad move, remove it as an option
+    if head_pos_y == top_edge:
+        up_clear = False
+    else:
+        up_element = board_matrix[head_pos_y+1][head_pos_x]
+        if up_element in danger_elements:
+            up_clear = False
+
+    #if down is a bad move, remove it as an option
+    if head_pos_y == bottom_edge:
+        down_clear = False
+    else:
+        down_element = board_matrix[head_pos_y-1][head_pos_x]
+        if down_element in danger_elements:
+            down_clear = False
+
+    #if left is a bad move, remove it as an option
+    if head_pos_x == left_edge:
+        left_clear = False
+    else:
+        left_element = board_matrix[head_pos_y][head_pos_x-1]
+        if left_element in danger_elements:
+            left_clear = False
+
+    #if right is a bad move, remove it as an option
+    if head_pos_x == right_edge:
+        right_clear = False
+    else:
+        right_element = board_matrix[head_pos_y][head_pos_x+1]
+        if right_element in danger_elements:
+            right_clear = False
+
+    #now to update the list of safe moves
+    if up_clear:
+        safe_choices.append('up')
+
+    if down_clear:
+        safe_choices.append('down')
+
+    if left_clear:
+        safe_choices.append('left')
+
+    if right_clear:
+        safe_choices.append('right')
+
+    return safe_choices
+
+def hunting(safe_choices, board_food,head_pos_x,head_pos_y):
+    food_location = food_finder(board_food,head_pos_x,head_pos_y)
+    food_x = food_location[0]
+    food_y = food_location[1]
+
+    if head_pos_x != food_x:
+        if (head_pos_x > food_x):
+            #if head_pos_x is > food, head is to the right of food
+            if 'left' in safe_choices:
+                return 'left'
+            else:
+                #check y alignment and see if thats a move otherwise do first
+                #element in safe_choices
+                if head_pos_y > food_y:
+                    #if head > food_y, go down check
+                    if 'down' in safe_choices:
+                        return 'down'
+                    else:
+                        return safe_choices[0]
+                else:
+                    #if head < food_y, go up check
+                    if 'up' in safe_choices:
+                        return 'down'
+                    else:
+                        return safe_choices[0]
         else:
-            print "find_element_index_2d Working"
-            print "x:%d y:%d"%(x,y)
+            #if head_pos_x is < food, head is to the right of food
+            if 'right' in safe_choices:
+                return 'right'
+            else:
+                #check y alignment and see if thats a move otherwise do first
+                #element in safe_choices
+                if head_pos_y > food_y:
+                    #if head > food_y, go down check
+                    if 'down' in safe_choices:
+                        return 'down'
+                    else:
+                        return safe_choices[0]
+                else:
+                    #if head < food_y, go up check
+                    if 'up' in safe_choices:
+                        return 'down'
+                    else:
+                        return safe_choices[0]
+    else:
+        #this is when it right above food
+        if head_pos_y > food_y:
+            #if head > food_y, go down check
+            if 'down' in safe_choices:
+                return 'down'
+            else:
+                return safe_choices[0]
+        else:
+            #if head < food_y, go up check
+            if 'up' in safe_choices:
+                return 'down'
+            else:
+                return safe_choices[0]
+
+def food_finder(board_food,head_pos_x,head_pos_y):
+    """
+    find the closest food in a list = [x,y]
+    """
+    food_amount = len(board_food)
+    closest_food = []
+    closest_distance = 0
+    current_distance = 0
+    x_delta = 0
+    y_delta = 0
+
+    while food_amount > 0:
+        food_token = board_food[food_amount-1]
+        food_x = food_token['x']
+        food_y = food_token['y']
+        x_delta = food_x - head_pos_x
+        x_delta = abs(x_delta)
+        y_delta = food_y - head_pos_y
+        y_delta = abs(y_delta)
+        current_distance = y_delta + x_delta
+        print "current_distance: %d, closest_distance: %d" %(current_distance, closest_distance)
+        if food_amount == len(board_food):
+            #first time in loop
+            closest_distance = current_distance
+
+        if current_distance <= closest_distance:
+            closest_food = [food_x,food_y]
+            closest_distance = current_distance
+            print "here"
+
+        food_amount -= 1
+
+    if debug:
+            if len(closest_food) != 2:
+                print "ERROR food_finder, food finder produced"
+                print (np.matrix(closest_food))
+            else:
+                print ("this is where the closest food is :")
+                print np.matrix(closest_food)
+    return closest_food
---
+++ run_after_first_time.command
@@ -0,0 +1,3 @@
+cd
+cd documents/github/striper-snake/run
+open snake_run.command
