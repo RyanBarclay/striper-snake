b6d243b270ff4cb201cab998a1038ad61694ab67
RyanBarclay <mrryanbarclay@gmail.com>
2019-02-25 11:32:13 -0800

    Updated food_finder()
    Added todo on some of the funtions. Added more debug prints

--- app/brain.py
+++ app/brain.py
@@ -1,21 +1,17 @@
 import math
 import json
-# import numpy as np
+import numpy as np
 
-debug = False
+debug = True
 
 def think(data):
     """
     Function: think(data)
 
     Description:
-        This function is the primary function of this file. It's directive,
-        is itemize all of the json data passed through the input. Then pass
-        on the necessary data for each function. The functions will need to
-        find the nearest food, find out if its the closest to that food
-        compared to other snakes, decide if it wants to go for the food,
-        will make a 'closed box' around that food, when it is going to die
-        will eat food, make a holding pattern 'closed box', then repeat.
+        This function is the primary function of this file. It's directive, is to itemize all of the json data passed through the input. Then pass on the necessary data for each function.
+
+        The functions will need to find out if its the closest to that food compared to other snakes, decide if it wants to go for the food, will make a 'closed box' around that food, when it is going to die will eat food, make a holding pattern 'closed box', then repeat.
 
     Input:
         data:
@@ -86,15 +82,21 @@ def think(data):
     you_health = you['health']
     you_body = you['body']
 
+    if debug:
+        print(you_name)
+
     board_matrix = [[0 for x in range(board_width)] for y in range(board_height)]
     # this will make an array elements of widths. ie
     look(board_food, board_snakes, you_id, board_matrix, turn)
+    food_location, closest = food_finder(board_food, you_id, board_snakes)
 
-    if debug == True:
+    if debug:
+        # print("think debug:")
         # print(np.matrix(board_matrix))
-        print("think debug")
+        pass
 
-    return instincts(board_matrix, board_food)
+    return 'up'
+    # return instincts(board_matrix, board_food)
 
 def look(board_food, board_snakes, you_id, board_matrix, turn):
 
@@ -161,40 +163,6 @@ def look(board_food, board_snakes, you_id, board_matrix, turn):
                     board_matrix[cur_snake_body_y][cur_snake_body_x] ='b'
             i += 1
         board_snakes_amount -= 1
-    #end of look function
-def instincts(board_matrix, board_food):
-
-    # will find my head and tell me where it is and exit x,y
-    head_pos_y = len(board_matrix)-1
-    head_token = board_matrix[0]
-    head_pos_x = len(head_token)-1
-    x_max = head_pos_x
-    element = 'mh'
-    end = False
-    safe_choices = []
-
-    # this finds the head .... hopefully no bugs
-    while head_pos_y >= 0:
-        while head_pos_x >= 0:
-            if board_matrix[head_pos_y][head_pos_x] == element:
-                end = True
-                break
-            else:
-                head_pos_x -= 1
-        if end == True:
-            break
-        else:
-            head_pos_y -= 1
-            head_pos_x = x_max
-    # end of find head loop
-
-    if debug == True:
-        print "head of snake x:%d ,y:%d" %(head_pos_x,head_pos_y)
-
-    safe_choices = move_check(safe_choices, head_pos_x, head_pos_y, board_matrix)
-
-
-    return hunting(safe_choices, board_food,head_pos_x,head_pos_y)
 
 def move_check(safe_choices, head_pos_x, head_pos_y, board_matrix):
     """
@@ -202,6 +170,10 @@ def move_check(safe_choices, head_pos_x, head_pos_y, board_matrix):
     as a list of those moves as strings acceptable as /move entries
 
     If there are no safe_choices left up will be the default choice
+    """
+    
+    """
+    TODO: Update to new style of funtion passing
     """
 
     temp_token = board_matrix[0]
@@ -270,7 +242,144 @@ def move_check(safe_choices, head_pos_x, head_pos_y, board_matrix):
 
     return safe_choices
 
+def food_finder(board_food, you_id, board_snakes):
+    """
+    This function should return food Coords in a list = [x,y] this food will be the food that gives the  value to the following formula:
+    this works because the value with
+
+    if no food on board then will return empty list and a flase on the boolian
+
+    Input:
+        board_food:
+            list of { {[x: #][y: #]}, ...}
+        you_id:
+            id of my snake as a string
+        board_snakes:
+            reffer to think(date) explanation
+    """
+    food_index = len(board_food) - 1
+    closest_in_comparison = False
+    closest_food = []
+    distance_list = []
+    #this is a list of {[mysnake_distance, closest_snake_distnace]} elements
+
+    if len(board_food) == 0:
+        # if no food
+        return closest_food, closest_in_comparison
+
+    while food_index >= 0:
+        #cycle through food
+        food_token = board_food[food_index]
+        food_x = food_token['x']
+        food_y = food_token['y']
+        element = [0,0]
+        #initializes elements to be loaded into distance_list
+
+        snake_index = len(board_snakes) - 1
+        while snake_index >= 0:
+            snake_token = board_snakes[snake_index]
+            cur_snake_body = snake_token['body']
+            cur_snake_body_token = cur_snake_body[0]
+            #cur_snake_body_token = head
+
+            cur_snake_head_x = cur_snake_body_token['x']
+            cur_snake_head_y = cur_snake_body_token['y']
+            cur_snake_id = snake_token['id']
+
+            delta_x = abs(food_x - cur_snake_head_x)
+            delta_y = abs(food_y - cur_snake_head_y)
+            diffrence = delta_x + delta_y
+
+            if cur_snake_id  == you_id:
+                #if my snake will put in how far it is from the food
+                element[0] = diffrence
+
+            else:
+                #else find closest ememy snake to food
+                if element[1] == 0:
+                    #if first enemy snake it by deff is the closest ememy
+                    element[1] = diffrence
+
+                if element[1] > diffrence:
+                    #if the this enemy snake is closer than the current closest one, replace it
+                    element[1] = diffrence
+            snake_index -= 1
+        distance_list.append(element)
+        food_index -= 1
+    # now there is a list of lists with my distance to food and the closest enemy to the food as elements
+    distance_list_index = len(distance_list)-1
+
+    while distance_list_index >= 0:
+        #this will find the largest x value as stated above
+        token = distance_list[distance_list_index]
+        mysnake_distance = token[0]
+        enemy_distance = token[1]
+        x = mysnake_distance - enemy_distance
+        if distance_list_index == (len(distance_list)-1):
+            #first time in loop
+            largest_x = x
+            closest_food = board_food[distance_list_index]
+
+        if x > largest_x:
+            largest_x = x
+            closest_food = board_food[distance_list_index]
+        distance_list_index -= 1
+    if largest_x > 0:
+        closest_in_comparison = True
+    else:
+        closest_in_comparison = False
+
+    if debug:
+            print("This is the list distance_list:")
+            print(np.matrix(distance_list))
+            print("This is the return entries of food_finder:")
+            print("closest_food: %s" % (closest_food))
+            print("closest_in_comparison: %s" % (closest_in_comparison))
+
+    return closest_food, closest_in_comparison
+
+def instincts(board_matrix, board_food):
+    """
+    TODO: Retire this funtion
+    """
+
+    # will find my head and tell me where it is and exit x,y
+    head_pos_y = len(board_matrix)-1
+    head_token = board_matrix[0]
+    head_pos_x = len(head_token)-1
+    x_max = head_pos_x
+    element = 'mh'
+    end = False
+    safe_choices = []
+
+
+    # this finds the head .... hopefully no bugs
+    while head_pos_y >= 0:
+        while head_pos_x >= 0:
+            if board_matrix[head_pos_y][head_pos_x] == element:
+                end = True
+                break
+            else:
+                head_pos_x -= 1
+        if end == True:
+            break
+        else:
+            head_pos_y -= 1
+            head_pos_x = x_max
+    # end of find head loop
+
+    if debug == True:
+        print "head of snake x:%d ,y:%d" %(head_pos_x,head_pos_y)
+
+    safe_choices = move_check(safe_choices, head_pos_x, head_pos_y, board_matrix)
+
+
+    return hunting(safe_choices, board_food,head_pos_x,head_pos_y)
+
 def hunting(safe_choices, board_food, head_pos_x, head_pos_y):
+    """
+    TODO: Retire this funtion
+    """
     food_location = food_finder(board_food, head_pos_x, head_pos_y)
     if len(food_location) == 0:
         # if no food on board makes fake food in top left
@@ -338,59 +447,3 @@ def hunting(safe_choices, board_food, head_pos_x, head_pos_y):
                 return 'down'
             else:
                 return safe_choices[0]
-
-def food_finder(board_food, head_pos_x, head_pos_y):
-    """
-    This function should return food Coords in a list = [x,y] this food will be the food that gives the smallest value to the following formula:
-    x = (distance from my head to food) + (distance from closest ememy snake head to that food)
-    This should make it so the snake knows what food it is most likely to get. Moreover, this function will also return a boolian value to tell code if it is the closest to this food ie if y is +/-:
-    y = (distance from my head to selected food) - (distance from closest ememy snake head to that food)
-    if y + it is the closest compared to all other snakes
-
-    Input:
-        board_food:
-            list of { {[x: #][y: #]}, ...}
-        you_id:
-            id of my snake as a string
-        board_snakes:
-            reffer to think(date) explanation
-    """
-    food_amount = len(board_food)
-    closest_food = []
-    closest_distance = 0
-    current_distance = 0
-    x_delta = 0
-    y_delta = 0
-
-    if food_amount == 0:
-        return closest_food
-
-    while food_amount > 0:
-        food_token = board_food[food_amount-1]
-        food_x = food_token['x']
-        food_y = food_token['y']
-        x_delta = food_x - head_pos_x
-        x_delta = abs(x_delta)
-        y_delta = food_y - head_pos_y
-        y_delta = abs(y_delta)
-        current_distance = y_delta + x_delta
-        #print "current_distance: %d, closest_distance: %d" %(current_distance, closest_distance)
-        if food_amount == len(board_food):
-            #first time in loop
-            closest_distance = current_distance
-
-        if current_distance <= closest_distance:
-            closest_food = [food_x,food_y]
-            closest_distance = current_distance
-
-        food_amount -= 1
-
-    if debug:
-            if len(closest_food) != 2:
-                #if the length of the array closest_food isn't 2 then something went wrong
-                print "ERROR food_finder, food finder produced"
-                # print (np.matrix(closest_food))
-            else:
-                print ("this is where the closest food is :")
-                # print np.matrix(closest_food)
-    return closest_food
